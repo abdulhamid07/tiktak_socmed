783d942bb770c8cd7d2be9025b44be0d84617f77
abdulhamid07 <abdulhamid.dev@gmail.com>
2022-11-20 09:59:59 +0700

    ...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
+  
   reactStrictMode: true,
   images: {
     domains: ["placeimg.com", "lh3.googleusercontent.com"],
